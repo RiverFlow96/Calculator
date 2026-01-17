3e7e8c41d73b35b531913ffaccf7e90b660dca6e
RiverFlow96 <yordanyalmeda8@gmail.com>
2026-01-17 10:52:03 -0500

    Implementacion de Redux en los componentes, Agregado disenio al contador

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import './App.css'
+import { useSelector, useDispatch } from 'react-redux'
+import { incrementar, decrementar, incrementarPorMontos} from './features/counter/counterSlice'
 
 function App() {
 
+  const count = useSelector((state: any) => state.counter.value)
+  const dispatch = useDispatch()
+
   return (
-    <>
-      <div className='w-2xl h-2xl bg-red-600'>Hola Mundo</div>
-    </>
+    <div className='bg-white shadow-2xl w-[40dvw] h-[60dvh] flex flex-col justify-center items-center gap-4 rounded-2xl font-sans'>
+      <h1 className='m-0 p-0 font-bold'>Contador:</h1>
+      <p className='text-2xl p-0 m-0'>{count}</p>
+
+      <div className='flex flex-row gap-4'>
+        <button className='bg-gray-700 w-10 h-10 rounded-2xl text-white' onClick={() => dispatch(incrementar())}> +1 </button>
+
+        <button className='bg-gray-700 w-10 h-10 rounded-2xl text-white' onClick={() => dispatch(decrementar())}> -1 </button>
+
+        <button className='bg-gray-700 w-10 h-10 rounded-2xl text-white' onClick={() => dispatch(incrementarPorMonto(5))}> +5 </button>
+      </div>
+    </div>
   )
 }
 
---
+++ src/features/counter/counterSlice.ts
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const counterSlice = createSlice({
+    name: 'counter',
+    initialState: {
+        value: 0,
+    },
+    reducers: {
+        incrementar: (state) => {
+            // Redux Toolkit nos permite "mutar" el estado de forma segura aquí
+            state.value += 1;
+        },
+        decrementar: (state) => {
+            state.value -= 1;
+        },
+        incrementarPorMonto: (state, action) => {
+            // Usamos action.payload para recibir un valor externo
+            state.value += action.payload;
+        },
+        imprimir_valor(state) {
+            console.log("Valor actual del contador:", state.value);
+        }
+    },
+});
+
+// Exportamos las acciones para usarlas en los componentes
+export const { incrementar, decrementar, incrementarPorMonto } = counterSlice.actions;
+
+// Exportamos el reducer para el store
+export default counterSlice.reducer;
