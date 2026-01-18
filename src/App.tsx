1663450a41ee11f937c14b4d62994388dbc4cc0a
Dayron Mustelier <dayrondotres@gmail.com>
2026-01-18 23:49:00 +0100

    Fixed container constraints & button styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css'
 import { useSelector, useDispatch } from 'react-redux'
 import { incrementar, decrementar, incrementarPorMonto } from './features/counter/counterSlice'
 import type { RootState, AppDispatch } from './Redux/store'
@@ -12,16 +11,16 @@ function App() {
   const dispatch = useDispatch<AppDispatch>()
 
   return (
-    <div className='bg-white shadow-2xl w-[40dvw] h-[60dvh] flex flex-col justify-center items-center gap-4 rounded-2xl font-sans'>
+    <div className='bg-white shadow-2xl w-[40dvw] h-[60dvh] flex flex-col justify-center items-center gap-4 rounded-2xl font-sans min-w-90'>
       <h1 className='m-0 p-0 font-bold'>Contador:</h1>
       <p className='text-2xl p-0 m-0'>{count}</p>
 
       <div className='flex flex-row gap-4'>
-        <button className='bg-gray-700 w-10 h-10 rounded-2xl text-white' onClick={() => dispatch(incrementar())}> +1 </button>
+        <button className='bg-gray-700 w-10 h-10 rounded-2xl text-white hover:bg-gray-900' onClick={() => dispatch(incrementar())}> +1 </button>
 
-        <button className='bg-gray-700 w-10 h-10 rounded-2xl text-white' onClick={() => dispatch(decrementar())}> -1 </button>
+        <button className='bg-gray-700 w-10 h-10 rounded-2xl text-white hover:bg-gray-900' onClick={() => dispatch(decrementar())}> -1 </button>
 
-        <button className='bg-gray-700 w-10 h-10 rounded-2xl text-white' onClick={() => dispatch(incrementarPorMonto(5))}> +5 </button>
+        <button className='bg-gray-700 w-10 h-10 rounded-2xl text-white hover:bg-gray-900' onClick={() => dispatch(incrementarPorMonto(5))}> +5 </button>
       </div>
     </div>
   )
